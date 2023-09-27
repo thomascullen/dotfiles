8baceae286a3c29e526e807719beffb7c0b132c2
Thomas Cullen <thomascullen92@gmail.com>
2023-09-27 23:24:36 +0100

    Update

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  },
+}
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,26 +1,41 @@
 return {
-  'loctvl842/monokai-pro.nvim',
-  priority = 100,
+  "folke/tokyonight.nvim",
   lazy = false,
-  name = 'monokai-pro',
-  config = function()
-    require('monokai-pro').setup({
-      transparent_background = true,
-      filter = "ristretto",
-      inc_search = "background",
-      background_clear = {
-        "telescope",
-        "toggleterm",
-      },
-      override = function(c)
-        return {
-          Directory = { fg = "none", bg = "none" },
-          TelescopeSelection = { fg = "white", bg = "#3C3434" },
-          SagaBorder = { fg = c.base.yellow }
-        }
-      end
-    })
-
-    vim.cmd('colorscheme monokai-pro')
-  end,
+  priority = 1000,
+  opts = {
+    style = "night",
+    transparent = false,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd[[colorscheme tokyonight]]
+  end
 }
+--
+--
+-- return {
+--   'loctvl842/monokai-pro.nvim',
+--   priority = 100,
+--   lazy = false,
+--   name = 'monokai-pro',
+--   config = function()
+--     require('monokai-pro').setup({
+--       transparent_background = true,
+--       filter = "ristretto",
+--       inc_search = "background",
+--       background_clear = {
+--         "telescope",
+--         "toggleterm",
+--       },
+--       override = function(c)
+--         return {
+--           Directory = { fg = "none", bg = "none" },
+--           TelescopeSelection = { fg = "white", bg = "#3C3434" },
+--           SagaBorder = { fg = c.base.yellow }
+--         }
+--       end
+--     })
+--
+--     vim.cmd('colorscheme monokai-pro')
+--   end,
+-- }
