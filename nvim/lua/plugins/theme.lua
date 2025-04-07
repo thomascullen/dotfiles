5a1b741cb278bd4ef2e5aa1ecb95b1e1fe107753
Thomas Cullen <thomascullen92@gmail.com>
2025-04-07 22:32:07 +0100

    Theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,7 +1,15 @@
 return {
-  "EdenEast/nightfox.nvim",
-  config = function(_, opts)
-    vim.cmd [[colorscheme terafox]]
+  "zenbones-theme/zenbones.nvim",
+  -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+  -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+  -- In Vim, compat mode is turned on as Lush only works in Neovim.
+  dependencies = "rktjmp/lush.nvim",
+  lazy = false,
+  priority = 1000,
+  -- you can set set configuration options here
+  config = function()
+    vim.g.rosebones_darkness = 'stark'
+    vim.cmd.colorscheme('rosebones')
   end
 }
 --
