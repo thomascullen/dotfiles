20355d33589f6bc875ddfb90bf38cfe6b695b4be
Thomas Cullen <thomascullen92@gmail.com>
2025-04-09 21:57:39 +0100

    theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,16 +1,11 @@
 return {
-  "zenbones-theme/zenbones.nvim",
-  -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-  -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  -- In Vim, compat mode is turned on as Lush only works in Neovim.
-  dependencies = "rktjmp/lush.nvim",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  -- you can set set configuration options here
-  config = function()
-    vim.g.rosebones_darkness = 'stark'
-    vim.cmd.colorscheme('rosebones')
-  end
+  opts = {},
+  config = function(_, opts)
+    vim.cmd [[colorscheme tokyonight-night]]
+  end,
 }
 --
 -- return {
