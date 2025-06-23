e2ede3a2d2f9226835e20ece9064c8d9c45229c1
Thomas Cullen <thomascullen92@gmail.com>
2025-06-23 22:33:06 +0100

    Enable autoread

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = ' '
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
+opt.autoread = true  -- Auto update files when changed outside of Vim
 -- only set clipboard if not in ssh, to make sure the OSC 52
 -- integration works automatically. Requires Neovim >= 0.10.0
 -- opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
