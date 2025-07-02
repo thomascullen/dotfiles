880c7a09da3aacdf4fe44f93d9aa03ab31661fea
Thomas Cullen <thomas@evervault.com>
2025-07-02 13:02:44 +0100

    remove copilot cmp

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,13 +1,9 @@
 return {
   "zbirenbaum/copilot.lua",
-  dependencies = {
-    "zbirenbaum/copilot-cmp",
-  },
   config = function()
     require("copilot").setup({
       suggestion = { enabled = false },
       panel = { enabled = false },
     })
-    require("copilot_cmp").setup()
   end
 }
