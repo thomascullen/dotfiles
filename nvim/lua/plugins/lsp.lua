b419f89254e3a33ac02d5686e27b6d941454c124
Thomas Cullen <thomascullen92@gmail.com>
2026-01-27 17:36:16 +0000

    Add gd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,20 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-  opts = {},
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
     "neovim/nvim-lspconfig",
   },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(event)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+          buffer = event.buf,
+          desc = "Go to definition",
+        })
+      end,
+    })
+  end,
 }
