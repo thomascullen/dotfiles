a0a9a74b72b465af867257800af762a7db5a0766
Thomas Cullen <thomascullen92@gmail.com>
2026-04-23 23:29:56 +0100

    Enable wrap for markdown files

--- nvim/init.lua
+++ nvim/init.lua
@@ -116,3 +116,8 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown' },
+  callback = function() vim.opt_local.wrap = true end,
+})
